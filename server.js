140d00e500b3c5d38a4c69200bdf3d83b3c9af17
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-03 01:42:05 -0400

    Finished up the code for the functions in the HTML files, so that the reservations and waitlists can be made and properly appended to the page. Had originally directed the information to append in the wrong div, which is why the information didn't propery populate the DOM. Also had issues with my API routes as I wasn't calling on the empty arrays correctly, so I corrected that.Unfortunately, though my routes and functions are working appropriately, the information populating the page is undefined, so I must look into the issue further.

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 // Allows the user to receive the information back in JSON form.
 const bodyParser = require('body-parser')
+
 // Makes routing easier for the user using node.js.
 const express = require("express");
+
 // Setting the variable in order to easily call it.
 const app = express();
 const path = require("path");
+
 // The port can be whatever the port is defined as by the deployment site or it also works on the local host (so that it doesn't need to be reconfigured when deployed live).
 const PORT = process.env.PORT || 2187;
 
-// Sets up the Express app to handle data parsing
+// Sets up the Express app to handle data parsing.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const reservations = [];
 const waitlist = [];
 
-// Routes
+// HTML Routes
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -30,30 +33,27 @@ app.get("/reserve", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/reservation.html"))
 });
 
-// API Pages
-
+// API Routes
 app.get("/api/tables", (req, res) => {
-    res.json(reservations);
+    return res.json(reservations);
 });
 
 app.post("/api/tables", (req, res) => {
-    const newObj = req.body;
+    var newReservation = req.body;
 
-    console.log(newObj);
-
-    reservations.push(newObj);
-
-    console.log(reservations);
-
-    // Indicates that we are done with our backend code and kicks off to .then of post function
-    res.json(newObj);
+    if(reservations.length >= 5)    {
+        waitlist.push(newReservation);
+    } else  {
+        reservations.push(newReservation);
+    }
+    res.json(newReservation);
 });
 
 app.get("/api/waitlist", (req, res) => {
-    res.json(waitlist);
+    return res.json(waitlist);
 });
 
 app.listen(PORT, () => {
     // Lets the user know that the port is listening and functioning as expected.
-    console.log("Server is running on http://localhost:2187/")    
+    console.log("Server is running on 2187...Cell 2187, that is. Found in Detention Block AA-23, sub-level five.")    
 });
